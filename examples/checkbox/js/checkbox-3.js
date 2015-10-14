6312e2ca99acdc8d608094c98f0d6907e627ab42
a11ydoer <a11ydoer@gmail.com>
2015-10-14 16:35:17 -0500

    checkbox3 revision

--- examples/checkbox/js/checkbox-3.js
+++ examples/checkbox/js/checkbox-3.js
@@ -1,3 +1,20 @@
+window.addEventListener('load', function() {
+
+  var checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+  for(var i = 0; i < checkboxes.length; i++ ) {
+    var cb = checkboxes[i];
+
+    console.log(cb.tagName + " " + cb.id)
+
+    cb.addEventListener('change', updateGroupCheckbox);
+    cb.addEventListener('focus', focusStandardCheckbox);
+    cb.addEventListener('blur', blurStandardCheckbox);
+  }
+
+});
+
+
 /*
 * @function toggleGroupCheckBox
 *
@@ -20,52 +37,18 @@ function toggleGroupCheckbox(event) {
           if (state === 'false' || state === 'mixed') {
             node.setAttribute('aria-checked', 'true')
             image.src = './images/checkbox-checked-black.png'
-            setCheckboxes(node, 'true')
+            setCheckboxes(node, true)
           }
           else {
             node.setAttribute('aria-checked', 'false')
             image.src = './images/checkbox-unchecked-black.png'
-            setCheckboxes(node, 'false')
+            setCheckboxes(node, false)
           }  
 
     event.preventDefault()
     event.stopPropagation()
 
    } 
-
-}
-
-/*
-* @function toggleCheckBox
-*
-* @desc Togles the state of a groupbox and checkboxes and updates the image indicating state based on aria-checked values
-*
-* @param   {Object}  event  -  Standard W3C event object
-*
-*/
-
-function toggleCheckbox(event) {
-  var node = event.currentTarget
-
-  var image = node.getElementsByTagName('img')[0]
-
-  var state = node.getAttribute('aria-checked').toLowerCase()
-
-  if (event.type === 'click' || 
-      (event.type === 'keydown' && event.keyCode === 32)) {
-          if (state === 'true') {
-            node.setAttribute('aria-checked', 'false')
-            image.src = './images/checkbox-unchecked-black.png'
-          }
-          else {
-            node.setAttribute('aria-checked', 'true')
-            image.src = './images/checkbox-checked-black.png'
-          }  
-
-    updateGroupCheckbox(node)
-    event.preventDefault()
-    event.stopPropagation()
-  }
 
 }
 
@@ -81,20 +64,10 @@ function toggleCheckbox(event) {
 
 function setCheckboxes(node, state) {
 
-  var checkboxes = node.parentNode.getElementsByClassName('checkbox')
+  var checkboxes = node.parentNode.querySelectorAll('input[type=checkbox]')
 
   for (var i = 0; i < checkboxes.length; i++) {
-    var cb = checkboxes[i]
-
-    var image = cb.getElementsByTagName('img')[0]
-    cb.setAttribute('aria-checked', state)
-
-    if (state === 'false') {
-       image.src = './images/checkbox-unchecked-black.png'
-    }
-   else {
-     image.src = './images/checkbox-checked-black.png'
-    }  
+    checkboxes[i].checked = state;
   } 
 }
 
@@ -105,23 +78,25 @@ function setCheckboxes(node, state) {
 *
 * @param   {Object}   node  -  DOM node of updated group checkbox
 */
-function updateGroupCheckbox(node) {
+function updateGroupCheckbox(event) {
 
-  var checkboxes = node.parentNode.getElementsByClassName('checkbox')
+  var node = event.currentTarget;
+
+  console.log(node.tagName + " " + node.id)
+
+  var checkboxes = node.parentNode.parentNode.querySelectorAll('input[type=checkbox]')
 
   var state = 'false';
   var count = 0;
 
   for (var i = 0; i < checkboxes.length; i++) {
-    var cb = checkboxes[i]
-
-    if (cb.getAttribute('aria-checked') === 'true') count += 1
+    if (checkboxes[i].checked) count += 1
   } 
 
   if (count > 0) state = 'mixed'
   if (count === checkboxes.length) state = 'true'  
 
-  var group_checkbox = node.parentNode.getElementsByClassName('group_checkbox')[0]   
+  var group_checkbox = node.parentNode.parentNode.getElementsByClassName('group_checkbox')[0]   
   var image = group_checkbox.getElementsByTagName('img')[0]
 
   group_checkbox.setAttribute('aria-checked', state)
@@ -155,3 +130,26 @@ function blurCheckbox(event) {
   event.currentTarget.className = event.currentTarget.className .replace(' focus','')
 }
 
+/*
+* @function focusStandardCheckBox
+*
+* @desc Adds focus styling to label element encapsulating standard checkbox
+*
+* @param   {Object}  event  -  Standard W3C event object
+*/
+
+function focusStandardCheckbox(event) {
+  event.currentTarget.parentNode.className += ' focus' ;
+}
+
+/*
+* @function blurStandardCheckBox
+*
+* @desc Adds focus styling to the label element encapsulating standard checkbox
+*
+* @param   {Object}  event  -  Standard W3C event object
+*/
+
+function blurStandardCheckbox(event) {
+  event.currentTarget.parentNode.className = event.currentTarget.parentNode.className .replace(' focus','');
+}
